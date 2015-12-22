b80cf6475bc5cb76a614161663e8dcdbbc62f03c
zhangqi <zhangqi@sagereal.com>
2015-12-22 14:15:36 +0800

    add RPGGameView

---
+++ Games/src/com/sagereal/zq/game/rpg/RPGameView.java
@@ -0,0 +1,19 @@
+package com.sagereal.zq.game.rpg;
+
+import android.content.Context;
+import com.sagereal.zq.game.BaseView;
+
+public class RPGameView extends BaseView{
+	
+
+	public RPGameView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initView() {
+		// TODO Auto-generated method stub
+		
+	}
+}
